d876b321bb467b6f82d5a3b52883a912297665ba
charizardmigo <fomechepriestly7@gmail.com>
2021-08-11 03:17:24 +0100

    displays nike symbol

---
+++ chapter_2/nike_display.c
@@ -0,0 +1,16 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: TO DISPLAY NIKE SYMBOL
+//DATE: 14th JULY, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("         *\n");
+    printf("        *\n");
+    printf("       *\n");
+    printf("  *   *\n");
+    printf("   * *\n");
+    printf("    *\n");
+}
