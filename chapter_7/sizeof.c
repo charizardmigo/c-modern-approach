78255deb8790e50abc6c772f11e9bb2a521b41b1
charizardmigo <fomechepriestly7@gmail.com>
2021-09-20 14:48:34 +0100

    prints size of variable types

---
+++ chapter_7/sizeof.c
@@ -0,0 +1,18 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: PRINTS THE SIZE OF DIFFERENT VARIABLE TYPES
+//DATE: 6TH AUGUST, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Size of int is %d\n", (int) sizeof (int));
+    printf("Size of short is %d\n", (int) sizeof (short));
+    printf("Size of long is %d\n", (int) sizeof (long));
+    printf("Size of float is %d\n", (int) sizeof (float));
+    printf("Size of double is %d\n", (int) sizeof (double));
+    printf("Size of long double is %d\n", (int) sizeof (long double));
+
+    return 0;
+}
